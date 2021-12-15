a1bd4f5f44af54fd6d831ffb93789809354baeb7
AN1BRA <bzakaria@anibra.dev>
2021-12-15 16:58:21 +0100

    add test cases for signature validation

---
+++ tests/Webhook/WebhookTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Webhook;
+
+use Tests\BaseTestCase;
+use YouCan\Pay\YouCanPay;
+
+class WebhookTest extends BaseTestCase
+{
+    public function test_valid_signature_check()
+    {
+        $youcanPay = YouCanPay::instance();
+
+        $privateKey = 'pri_t0talIy-l9g1t-aNd-Va|1d-UU1D';
+        $payload = $this->getWebhookPayload();
+
+        $signature = hash_hmac(
+            'sha256',
+            $payload,
+            $privateKey,
+            false
+        );
+
+        $result = $youcanPay->verifyWebhookSignature($signature, $payload, $privateKey);
+
+        $this->assertTrue($result);
+    }
+
+    public function test_invalid_signature_check()
+    {
+        $youcanPay = YouCanPay::instance();
+
+        $privateKey = 'pri_t0talIy-l9g1t-aNd-Va|1d-UU1D';
+        $payload = $this->getWebhookPayload();
+
+        $signature = hash_hmac(
+            'sha256',
+            $payload,
+            $privateKey,
+            false
+        );
+
+        $result = $youcanPay->verifyWebhookSignature($signature, $payload, 'invalid_private_key');
+
+        $this->assertFalse($result);
+    }
+
+    private function getWebhookPayload(): string 
+    {
+        return json_encode([
+            "event_id"      => 9999,
+            "event_name"    => "transaction.success",
+            "payload"       => [
+                "transaction"   => [
+                    "id"        => "transaction_id_123",
+                    "status"    => 9001,
+                    "order_id"  => "order_id_123",
+                    "amount"    => "100.00",
+                    "currency"  => "MAD",
+                ],
+                "metadata"      => [
+                    "custom_field_1" => "custom_value_1",
+                    "custom_field_2" => false,
+                    "custom_field_3" => [
+                        "custom_value_3_1",
+                        "custom_value_3_2",
+                        "custom_value_3_3",
+                    ],
+                ],
+            ]
+        ]);
+    }
+}
