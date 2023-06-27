83caff779a81c6571aaec8fbf4a918c84dd8b3c2
y-abderrazik <yabderrazik@youcan.shop>
2023-06-27 10:16:20 +0100

    ServerException inherit from the BaseException

--- src/API/Exceptions/ServerException.php
+++ src/API/Exceptions/ServerException.php
@@ -5,6 +5,6 @@ namespace YouCan\Pay\API\Exceptions;
 /**
  * Exception when a server error is encountered (5xx codes)
  */
-class ServerException extends BadResponseException
+class ServerException extends ClientException
 {
 }
